cd7d23bd8b699f75699da5e00924696f7d65a558
Andrew Harvey <ah.harvey@gmail.com>
2012-03-09 17:12:37 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elvin::Application.config.secret_token = '35ff2adddafd456bd679c7b1e7e8da255081efe113b66849b75c730b4f16123377c99db12103cc562f0a17a74bb16cefb4ffa356382c01299f91df3d395c3c96'

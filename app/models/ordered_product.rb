a8de2918b20a51411f9fe5c141cc7fef67a448e3
elvin alvian siagian <elvin@41studio.com>
2012-03-12 22:36:10 +0700

    working on issue number 1 for missing find or create methods

--- app/models/ordered_product.rb
+++ app/models/ordered_product.rb
@@ -4,38 +4,44 @@ class OrderedProduct < ActiveRecord::Base
   has_many :colors
   
   accepts_nested_attributes_for :colors, 
-	                              :reject_if => proc {|attrs| attrs[:product_name].blank?},
-	                              :allow_destroy => true
+    :reject_if => proc {|attrs| attrs[:product_name].blank?},
+    :allow_destroy => true
 	                              
 	                             
   def product_name
-      product.try(:name)
+    product.try(:name)
   end
 
   def product_name=(name)
-      self.product = Product.find_or_create_by_name(name) if name.present?
+    obj_product = Product.find_or_initialize_by_name(name) if name.present?
+    self.product = if obj_product.new_record?
+      obj_product.save(validate: false)
+      obj_product
+    else
+      obj_product
+    end
   end
   
   private
-    def decrement_product_stock
-      if self.order.fullfilled?
-        self.product.decrement!(:stock, self.quantity )
-      end
+  def decrement_product_stock
+    if self.order.fullfilled?
+      self.product.decrement!(:stock, self.quantity )
     end
+  end
 
-    def increment_product_stock
-      if self.order.fullfilled?
-        self.product.increment!(:stock, self.quantity )
-      end
+  def increment_product_stock
+    if self.order.fullfilled?
+      self.product.increment!(:stock, self.quantity )
     end
+  end
     
-    def recalculate_product_stock
-      if self.order.fullfilled?
-        if self.order.fullfilled_changed?
-          self.product.increment!(:stock, self.quantity) 
-        else
-          self.quantity_changed? ? self.product.increment!(:stock, self.quantity - self.quantity_was) : ""
-        end
+  def recalculate_product_stock
+    if self.order.fullfilled?
+      if self.order.fullfilled_changed?
+        self.product.increment!(:stock, self.quantity)
+      else
+        self.quantity_changed? ? self.product.increment!(:stock, self.quantity - self.quantity_was) : ""
       end
     end
+  end
 end

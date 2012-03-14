2bd548ff84edd03ca0823feeea7b703a14cdb111
elvin alvian siagian <elvin@41studio.com>
2012-03-14 18:41:15 +0700

    overiding cocoon js for fixed issue number 2 point a and b

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,5 +7,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require cocoon
 //= require_tree .
+
--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,19 +1,27 @@
 $(function() { // enable autocomplete for ordered_products
-	$(document).on("focus",".product-name", function() {
-		$('.product-name').autocomplete({
-			source: $('.product-name').data('autocomplete-source')
-		}); 
-	});
+    $(document).on("focus",".product-name", function() {
+        $('.product-name').autocomplete({
+            source: $('.product-name').data('autocomplete-source')
+        });
+    });
 })
 
-$(function() { // set insert location for new ordered_product fields in order nested form 
+$(function() { // set insert location for new ordered_product fields in order nested form
+    if ($('#ordered_products_container').children().size() > 0){
+        var dom_html = '#ordered_products_container li.ordered-product:last'
+        var position = 'after'
+    }
+    else {
+        var dom_html = '#ordered_products_container'
+        var position = 'append'
+    }
     $("a.add-ordered-product").
-      data("association-insertion-position", 'append').
-      data("association-insertion-node", '#ordered_products_container');
+    data("association-insertion-position", position).
+    data("association-insertion-node", dom_html);
 });
 
 $(function() { // set location of new color fields in order nested form 
     $("a.add-color").
-      data("association-insertion-position", 'before').
-      data("association-insertion-node", "this");
+    data("association-insertion-position", 'append').
+    data("association-insertion-node");
 });

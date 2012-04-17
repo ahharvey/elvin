75fdc0a4ca2436b4a0831773820b73fd066efb09
elvin alvian siagian <elvin@41studio.com>
2012-04-17 13:22:09 +0700

    fixed bug on all group_user model

--- app/models/groupb_user.rb
+++ app/models/groupb_user.rb
@@ -9,7 +9,7 @@ class GroupbUser < ActiveRecord::Base
   def grpb_user_name=(name)
       obj_user = User.find_or_initialize_by_name(name) if name.present?
       self.user = if obj_user.new_record?
-        obj_user.email = name+"@test.com"
+        obj_user.email = name+"@test.com" if obj_user.email.blank?
         obj_user.save(:validate=>false)
         obj_user
       else
